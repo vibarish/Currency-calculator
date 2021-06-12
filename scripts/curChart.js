5ea5f7ad5cef1c10d1c1c0d3cb4b0c7fec776b86
Vasiliy <vibarish@gmail.com>
2021-06-12 19:09:48 +0300

    120621

--- scripts/curChart.js
+++ scripts/curChart.js
@@ -29,60 +29,66 @@ console.log('даты', datesArr);
 
 const ctx = document.getElementById("myChart").getContext("2d");
 
-const myChart = new Chart(ctx, {
-  type: "line",
-  data: {
-    labels: datesArr,
-    datasets: [
-      {
-        label: "курс доллара",
-        data: usdArr,
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          // 'rgba(54, 162, 235, 0.2)',
-          // 'rgba(255, 206, 86, 0.2)',
-          // 'rgba(75, 192, 192, 0.2)',
-          // 'rgba(153, 102, 255, 0.2)',
-          // 'rgba(255, 159, 64, 0.2)'
-        ],
-        borderColor: [
-          "rgba(255, 99, 132, 1)",
-          // 'rgba(54, 162, 235, 1)',
-          // 'rgba(255, 206, 86, 1)',
-          // 'rgba(75, 192, 192, 1)',
-          // 'rgba(153, 102, 255, 1)',
-          // 'rgba(255, 159, 64, 1)'
-        ],
-        borderWidth: 1,
-      },
-      {
-        label: "курс евро",
-        data: euroArr,
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          // 'rgba(255, 159, 64, 0.2)'
-        ],
-        borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          // 'rgba(255, 159, 64, 1)'
-        ],
-        borderWidth: 1,
-      },
-    ],
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: false,
-      },
+window.onload = () => {
+  const myChart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: datesArr,
+      datasets: [
+        {
+          label: "курс доллара",
+          data: usdArr,
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            // 'rgba(54, 162, 235, 0.2)',
+            // 'rgba(255, 206, 86, 0.2)',
+            // 'rgba(75, 192, 192, 0.2)',
+            // 'rgba(153, 102, 255, 0.2)',
+            // 'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+            // 'rgba(54, 162, 235, 1)',
+            // 'rgba(255, 206, 86, 1)',
+            // 'rgba(75, 192, 192, 1)',
+            // 'rgba(153, 102, 255, 1)',
+            // 'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1,
+        },
+        {
+          label: "курс евро",
+          data: euroArr,
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            // 'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            // 'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1,
+        },
+      ],
     },
-  },
-});
+    options: {
+      scales: {
+        y: {
+          beginAtZero: false,
+        },
+      },
+      responsive: true,
+      maintainAspectRatio: false,
+      
+    },
+  });
+}
+
